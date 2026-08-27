4b9d26092d2afcb5d514c031d0a21ac8288010cb
aaurix <0xmassis@gmail.com>
2026-08-27 23:53:41 +0800

    test: make nightly property profile explicit (#285)

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,9 @@ settings.register_profile("fast", max_examples=40, stateful_step_count=20, datab
 settings.register_profile(
     "ci", max_examples=150, stateful_step_count=50, database=None, derandomize=True, print_blob=True
 )
-settings.register_profile("nightly", max_examples=500, stateful_step_count=100, database=None, print_blob=True)
+settings.register_profile(
+    "nightly", max_examples=500, stateful_step_count=100, database=None, derandomize=False, print_blob=True
+)
 settings.load_profile(
     os.environ.get("TRACEFOLD_HYPOTHESIS_PROFILE")
     or ("ci" if os.environ.get("TRACEFOLD_TEST_EVIDENCE") == "1" else "fast")
